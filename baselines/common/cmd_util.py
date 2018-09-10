67e774d1a3b650b64b401636ecf0a27bb49c077f
Pierre THODOROFF <pthodo@CS.McGill.CA>
2018-09-10 14:51:23 -0400

    add flags

--- baselines/common/cmd_util.py
+++ baselines/common/cmd_util.py
@@ -103,6 +103,7 @@ def common_arg_parser():
     parser.add_argument('--reward_scale', help='Reward scale factor. Default: 1.0', default=1.0, type=float)
     parser.add_argument('--save_path', help='Path to save trained model to', default=None, type=str)
     parser.add_argument('--beta', type=float, default=0)
+    parser.add_argument('--decay', type=float, default=0),
     parser.add_argument('--theta', type=float, default=0),
     parser.add_argument('--play', default=False, action='store_true')
     return parser
