19519c7bb98f61f38c5beae94593a376b890c0bb
Pierre THODOROFF <pthodo@CS.McGill.CA>
2018-09-13 11:23:16 -0400

    Add noise

--- utils/run_mujoco.sh
+++ utils/run_mujoco.sh
@@ -8,8 +8,9 @@ while IFS=, read beta theta decay lr noise_r;do
 		while read game
 		do
 			echo $game
-			python -m baselines.run --alg=ppo2  --noise_reward=$noise_r --env $game --decay=$decay --theta=$theta --lr=$lr --beta=$beta --seed=$s --network=mlp --num_timesteps=1e7 --identifier benchmark_var_5/ --path /home/ml/pthodo/RL/LOG/mujoco/ &
-		done < "./mujoco/swimmer_game_mujoco.txt"
+			python -m baselines.run --alg=ppo2  --noise_reward=$noise_r --env $game --decay=$decay --theta=$theta --lr=$lr --beta=$beta --seed=$s --network=mlp --num_timesteps=1e7 --identifier benchmark_var_8/ --path /home/ml/pthodo/RL/LOG/mujoco/ &
+		done < "./mujoco/game_mujoco.txt"
+		wait
 	done < "seeds.txt"
 done < "param_list.txt"
-wait
+
